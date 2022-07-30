3e61d8f85841139d93849295fa0b3ed66af99194
Diana Shnarevich <108484100+DianaShnarevich@users.noreply.github.com>
2022-07-30 11:16:10 +0300

    hw3

--- src/home_work_3/calcs/additional/CalculatorWithCounterAutoSuper.java
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoSuper.java
@@ -8,44 +8,37 @@ public class CalculatorWithCounterAutoSuper extends CalculatorWithMathExtends {
     public long getCountOperation() {
         return count;
     }
-
-    @Override
+    
     public double addition(double num1, double num2) {
         count++;
         return super.addition(num1, num2);
     }
 
-    @Override
     public double subtraction(double num1, double num2) {
         count++;
         return super.subtraction(num1, num2);
     }
 
-    @Override
     public double multiplication(double num1, double num2) {
         count++;
         return super.multiplication(num1, num2);
     }
 
-    @Override
     public double division(double num1, double num2) {
         count++;
         return super.division(num1, num2);
     }
-
-    @Override
+   
     public double exponentiation(double num1, int degree) {
         count++;
         return super.exponentiation(num1, degree);
     }
 
-    @Override
     public double module(double number) {
         count++;
         return super.module(number);
     }
 
-    @Override
     public double square_root(double number) {
         count++;
         return super.square_root(number);
