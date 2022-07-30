b077d8f960aff55824b0dccd9e76bd5654bf775c
Diana Shnarevich <108484100+DianaShnarevich@users.noreply.github.com>
2022-07-30 09:43:55 +0300

    Update WithoutCalculatorMain.java

--- src/home_work_3/runners/WithoutCalculatorMain.java
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -2,7 +2,7 @@ package home_work_3.runners;
 
 public class WithoutCalculatorMain {
     public static void main(String[] args) {
-        double result = ((4.1 + (15 * 7)) + (Math.pow((28 / 5), 2)));
+        double result = 4.1 + 15 * 7 + (28.0 / 5) * (28.0 / 5);
         System.out.println(result);
     }
 }
