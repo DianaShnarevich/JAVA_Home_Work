ed504dff151c11ca99cfa490744200f9f2224d9f
Diana Shnarevich <108484100+DianaShnarevich@users.noreply.github.com>
2022-07-30 10:33:58 +0300

    hw3

--- src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
+++ src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
@@ -4,22 +4,15 @@ import home_work_3.calcs.api.ICalculator;
 
 public class CalculatorWithMathExtends extends CalculatorWithOperator implements ICalculator {
 
-    @Override
     public double exponentiation(double num1, int degree){
         return Math.pow(num1, degree);
     }
 
-    @Override
     public double module(double number) {
         return Math.abs(number);
     }
 
-    @Override
     public double square_root(double number){
         return Math.sqrt(number);
     }
-
-
-
-
 }
